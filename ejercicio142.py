f257bc5db5a8346dd713c60cc8d7a0c3870b41c9
Judit <juditsr25@gmail.com>
2024-03-08 18:23:04 +0100

    final

--- ejercicio142.py
+++ ejercicio142.py
@@ -1,20 +1,5 @@
 import mysql.connector
 
-#Acceder al localhost y crear la base de datos.
-#mydb = mysql.connector.connect(
-#  host="localhost",
-#  user="root",
-#  password="",
-#)
-#mycursor = mydb.cursor()
-#mycursor.execute("CREATE DATABASE datosdb")
-
-#Comprobar que existe la base de datos
-#mycursor.execute("SHOW DATABASES")
-#for x in mycursor:
-#  print(x)
-
-#Conectarse a la base de datos
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
@@ -22,61 +7,22 @@ mydb = mysql.connector.connect(
   database="datosdb"
 )
 
-#Crear tabla
-#mycursor = mydb.cursor()
-#mycursor.execute("CREATE TABLE Datos (Nombre VARCHAR(255), Correo VARCHAR(255))")
-
-#Crear clave primaria
-#mycursor = mydb.cursor()
-#mycursor.execute("ALTER TABLE datos ADD PRIMARY KEY (Nombre)")
-
-#Insertar los datos
-#mycursor = mydb.cursor()
-#sql = "INSERT INTO datos (Nombre, Correo) VALUES (%s, %s)"
-#val = [
-#  ('Mercedes','mcast386@xtec.cat'),
-#  ('Rayane','rayane@rayane.sa'),
-#  ('Mohamed','moha@gmail.com'),
-#  ('Jad','jad@gmail.com'),
-#  ('Oriol','joam@gmail.com'),
-#  ('Elias','hola123@gmail.com'),
-#  ('Armau','arnau@gmail.com'),
-#  ('Asdrubal','asdrubal@gmail.com'),
-#  ('Adrian','pedrosanchez@asix2.com'),
-#  ('Eric','eric@gmail.com'),
-#  ('Emma','pacosanz@gmail.com'),
-#  ('nishwan','nishwan@gmail.com'),
-#  ('Javi','javi@gmail.com'),
-#  ('Novel','novelferreras49@gmail.com'),
-#  ('Bruno','elcigala@gmail.com'),
-#  ('David','argentino@gmail.com'),
-#  ('Judit','judit@gmail.com'),
-#  ('Joao','joao@gmail.com'),
-#  ('Laura','laura@gmail.com'),
-#  ('enrico','123@gmail.com'),
-#  ('Joel','joelcobre@gmail.com'),
-#  ('Aaron','aaron@gmail.com'),
-#  ('Moad','moad@gmail.com')
-#]
-#mycursor.executemany(sql, val)
-#mydb.commit()
-#print(mycursor.rowcount, "filas insertadas")
-
-def BuscarUsuario(nombrebuscar, contrasena): 
+def BuscarUsuario(correo, contrasena):
     mycursor = mydb.cursor()
-    #nombrebuscar = input ("Introduce un nombre para buscar su correo electrónico: ")
-    buscar = "SELECT Correo FROM datos WHERE Nombre = '" + nombrebuscar + "'" 
-    mycursor.execute(buscar, (nombrebuscar, contrasena))
-    myresult = mycursor.fetchall()
-    if myresult:
+    buscar = "SELECT correo, contrasena FROM datos WHERE correo = %s"
+    mycursor.execute(buscar, (correo,))
+    myresult = mycursor.fetchone()
+
+    if myresult and len(myresult) == 2 and myresult[1] == contrasena:
         return myresult[0]  # Retorna el correo encontrado
     else:
-        return "NOTROBAT"
+        return None
 
-def Añadir(nombre,correo, contrasena):
+
+def Añadir(nombre, correo, contrasena):
     mycursor = mydb.cursor()
     sql = "INSERT INTO datos (Nombre, Correo, Contrasena) VALUES (%s, %s, %s)"
-    val = (nombre, correo, contrasena)
+    val = (nombre, correo, contrasena,)
     mycursor.execute(sql, val)
     mydb.commit()
     print("Fila insertada correctamente")
@@ -88,21 +34,3 @@ def VerDatos():
     myresult = mycursor.fetchall()
     for x in myresult:
         print(x)
-
-#Menu
-while True:
-        print("\n1. Buscar correo")
-        print("2. Añadir usuario y correo")
-        print("3. Ver Tabla")
-        print("4. Salir")
-        opcion= (int)(input("Introduce una opción: "))
-        if opcion == 1:
-            BuscarUsuario()
-        elif opcion == 2:
-            Añadir()
-        elif opcion == 3:
-            VerDatos()
-        elif opcion == 4:
-            break
-        else:
-            print("Introduce un número válido")
--- webPF.py
+++ webPF.py
@@ -6,53 +6,61 @@ app.secret_key = 'BAD_SECRET_KEY'
 
 @app.route("/")
 def inici():
-    return redirect(url_for('getmail'))
+    return redirect(url_for('publica'))
 
-@app.route('/getmail',methods = ['POST', 'GET'])
+@app.route('/publica')
+def publica():
+        return render_template('paginasinlogin.html')
+
+@app.route('/getmail', methods=['POST', 'GET'])
 def getmail():
     if request.method == 'POST':
         correo = request.form['correo']
         contrasena = request.form['contrasena']
         usuario = ejercicio142.BuscarUsuario(correo, contrasena)
-        
+
         if usuario:
             session['email'] = correo
-            return redirect(url_for('webPF.html'))
+            return redirect(url_for('webpf'))
         else:
-            return render_template('resultgetmail.html', correo=correo)
+            error_message = "Credenciales incorrectas"
+            return render_template('iniciarsesionresult.html', correo=correo, error_message=error_message)
     else:
         return render_template('iniciarsesionPF.html')
+
+
 
 @app.route('/addmail', methods=['POST', 'GET'])
 def addmail():
     if request.method == 'POST':
+        nombre = request.form["nombre"]
         correo = request.form["correo"]
         contrasena = request.form["contrasena"]
-        ejercicio142.Añadir(correo, contrasena)
-        return render_template('resultaddmail.html', correo=correo, result_msg="Correo añadido correctamente")
+        ejercicio142.Añadir(nombre, correo, contrasena)
+        return render_template('resultadoanadirusuarioPF.html', correo=correo, result_msg="Correo añadido correctamente")
     else:
         return render_template('crearusuarioPF.html')
-
-#@app.route('/set_email', methods=['GET', 'POST'])
-#def set_email():
-#    if request.method == 'POST':
-#        session['email'] = request.form['email_address']
-#        return redirect(url_for('iniciarsesionPF.html'))
-
-#@app.route('/get_email')
-#def get_email():
-#    return render_template_string("""
-#            {% if session['email'] %}
-#                <h1>Bienvenido  {{ session['email'] }}!</h1>
-#            {% else %}
-#                <h1>Bienvenido Porfavor introduce tu correo <a href="{{ url_for('set_email') }}">aquí</a></h1>
-#            {% endif %}
-#        """)
 
 @app.route('/delete_email')
 def delete_email():
     session.pop('email', default=None)
-    return redirect(url_for('iniciarsesionPF.html'))
+    return redirect(url_for('publica'))
 
 if __name__ == '__main__':
     app.run()
+
+@app.route('/webpf')
+def webpf():
+    if 'email' in session:
+        return render_template('webpf.html', email=session['email'])
+    else:
+        return redirect(url_for('getmail'))
+
+@app.route('/formulario')
+def formulario():
+        return render_template('formularioPF2.html')
+
+
+@app.route('/aboutme')
+def aboutme():
+        return render_template('aboutmePF.html')
